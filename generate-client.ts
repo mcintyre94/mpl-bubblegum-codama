ae5162def2e13ac5157d5f98f63a8a8d9f22dba9
Callum <callum.mcintyre@anza.xyz>
2025-08-19 16:55:47 +0000

    Apply Codama anchor defaultVisitor

--- generate-client.ts
+++ generate-client.ts
@@ -1,8 +1,44 @@
-import { rootNodeFromAnchor, type AnchorIdl, rootNodeFromAnchorWithoutDefaultVisitor } from "@codama/nodes-from-anchor";
-import { assertIsNode, bottomUpTransformerVisitor, createFromRoot, definedTypeLinkNode, definedTypeNode, programNode, structFieldTypeNode, structTypeNode, updateProgramsVisitor } from "codama";
+import { type AnchorIdl, rootNodeFromAnchorWithoutDefaultVisitor } from "@codama/nodes-from-anchor";
+import { assertIsNode, bottomUpTransformerVisitor, createFromRoot, deduplicateIdenticalDefinedTypesVisitor, definedTypeLinkNode, definedTypeNode, flattenInstructionDataArgumentsVisitor, getCommonInstructionAccountDefaultRules, programNode, type RootNode, rootNodeVisitor, setFixedAccountSizesVisitor, setInstructionAccountDefaultValuesVisitor, structFieldTypeNode, structTypeNode, transformU8ArraysToBytesVisitor, unwrapInstructionArgsDefinedTypesVisitor, updateProgramsVisitor, visit, type Visitor } from "codama";
 import bubblegumIdl from "./idls/bubblegum.json" with { type: "json" };
 import { writeFileSync } from "node:fs";
 import path from "node:path";
+
+// Copied from https://github.com/codama-idl/codama/blob/efd160bec1aa8c6873135ba30aec78b621f2decb/packages/nodes-from-anchor/src/defaultVisitor.ts
+// Will be imported from Codama after https://github.com/codama-idl/codama/pull/780
+function defaultVisitor() {
+    return rootNodeVisitor(currentRoot => {
+        let root: RootNode = currentRoot;
+        const updateRoot = (visitor: Visitor<Node | null, 'rootNode'>) => {
+            const newRoot = visit(root, visitor);
+            // @ts-expect-error ignore for now
+            assertIsNode(newRoot, 'rootNode');
+            root = newRoot;
+        };
+
+        // Defined types.
+        // @ts-expect-error ignore for now
+        updateRoot(deduplicateIdenticalDefinedTypesVisitor());
+
+        // Accounts.
+        // @ts-expect-error ignore for now
+        updateRoot(setFixedAccountSizesVisitor());
+
+        // Instructions.
+        // @ts-expect-error ignore for now
+        updateRoot(setInstructionAccountDefaultValuesVisitor(getCommonInstructionAccountDefaultRules()));
+        // @ts-expect-error ignore for now
+        updateRoot(unwrapInstructionArgsDefinedTypesVisitor());
+        // @ts-expect-error ignore for now
+        updateRoot(flattenInstructionDataArgumentsVisitor());
+
+        // Extras.
+        // @ts-expect-error ignore for now
+        updateRoot(transformU8ArraysToBytesVisitor());
+
+        return root;
+    });
+}
 
 // Instantiate Codama without default visitor
 const codama = createFromRoot(
@@ -44,6 +80,9 @@ codama.update(
         }
     ])
 );
+
+// Apply the DefaultVisitor.
+codama.update(defaultVisitor());
 
 // Render tree.
 writeFileSync(
