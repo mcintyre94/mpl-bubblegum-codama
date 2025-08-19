0c81f47f68bf814ad4aef6d172b3f0da858a78f5
Callum <callum.mcintyre@anza.xyz>
2025-08-19 16:40:39 +0000

    Rename bubblegum to mplBubblegum

--- generate-client.ts
+++ generate-client.ts
@@ -1,13 +1,20 @@
 import { rootNodeFromAnchor, type AnchorIdl, rootNodeFromAnchorWithoutDefaultVisitor } from "@codama/nodes-from-anchor";
-import { createFromRoot } from "codama";
+import { createFromRoot, updateProgramsVisitor } from "codama";
 import bubblegumIdl from "./idls/bubblegum.json" with { type: "json" };
 import { writeFileSync } from "node:fs";
 import path from "node:path";
 
-
+// Instantiate Codama without default visitor
 const codama = createFromRoot(
     rootNodeFromAnchorWithoutDefaultVisitor(bubblegumIdl as AnchorIdl)
 );
+
+// Update programs.
+codama.update(
+    updateProgramsVisitor({
+        bubblegum: { name: "mplBubblegum" },
+    })
+)
 
 // Render tree.
 writeFileSync(
