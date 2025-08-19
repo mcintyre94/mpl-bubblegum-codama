7be0d78aa8281ebbfaa419d0cc06852582b0f8f7
Callum <callum.mcintyre@anza.xyz>
2025-08-19 16:47:54 +0000

    Add wrapper defined type for UpdateArgs

--- generate-client.ts
+++ generate-client.ts
@@ -1,5 +1,5 @@
 import { rootNodeFromAnchor, type AnchorIdl, rootNodeFromAnchorWithoutDefaultVisitor } from "@codama/nodes-from-anchor";
-import { createFromRoot, updateProgramsVisitor } from "codama";
+import { assertIsNode, bottomUpTransformerVisitor, createFromRoot, definedTypeLinkNode, definedTypeNode, programNode, structFieldTypeNode, structTypeNode, updateProgramsVisitor } from "codama";
 import bubblegumIdl from "./idls/bubblegum.json" with { type: "json" };
 import { writeFileSync } from "node:fs";
 import path from "node:path";
@@ -15,6 +15,35 @@ codama.update(
         bubblegum: { name: "mplBubblegum" },
     })
 )
+
+// Add wrapper defined type with a link to UpdateArgs. This is to avoid the
+// type being inlined in the instruction.
+codama.update(
+    bottomUpTransformerVisitor([
+        {
+            select: '[programNode]mplBubblegum',
+            transform: (node) => {
+                assertIsNode(node, "programNode");
+                return programNode({
+                    ...node,
+                    definedTypes: [
+                        ...node.definedTypes,
+                        // wrapper type
+                        definedTypeNode({
+                            name: "UpdateArgsWrapper",
+                            type: structTypeNode([
+                                structFieldTypeNode({
+                                    name: "wrapped",
+                                    type: definedTypeLinkNode("UpdateArgs"),
+                                })
+                            ])
+                        })
+                    ]
+                })
+            }
+        }
+    ])
+);
 
 // Render tree.
 writeFileSync(
